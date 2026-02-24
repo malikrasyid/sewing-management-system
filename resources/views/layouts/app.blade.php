e55820e5ca34ce9b255b14a88f3ee90077ff743a
malikrasyid <malikalrasyidi001@gmail.com>
2026-02-24 12:57:36 +0700

    fixing typo in layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/nexus.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/util.js'])
     <script src="https://unpkg.com/lucide@latest"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
